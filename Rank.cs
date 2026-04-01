a064e4a0a2b5082543b64a7bf1ce863bd5859df4
Thairan Govender <thairangov@gmail.com>
2026-04-01 17:12:35 +0200

    Delete Rank.cs

--- Rank.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlackJackGame
-{
-    public enum Rank
-    {
-        Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
-        Jack = 10, Queen = 10, King = 10, Ace = 11
-    }
-}
