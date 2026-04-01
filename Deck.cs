fe73ea17e1988350c0e578a29af5d48477cc67d8
Thairan Govender <thairangov@gmail.com>
2026-04-01 15:53:18 +0200

    fixed code

--- Deck.cs
+++ Deck.cs
@@ -1,40 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace BlackJackGame
+public class Deck
 {
-    public class Deck
+    public List<Card> Cards { get; private set; }
+    private Random rng = new Random();
+
+    public Deck()
     {
-        private List<Card> cards;
-        private Random rng = new Random();
-
-        public Deck()
+        Cards = new List<Card>();
+        foreach (Suit suit in Enum.GetValues(typeof(Suit)))
         {
-            cards = new List<Card>();
-            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            foreach (Rank rank in Enum.GetValues(typeof(Rank)))
             {
-                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
-                {
-                    cards.Add(new Card(suit, rank));
-                }
+                Cards.Add(new Card(suit, rank));
             }
         }
+    }
 
-        public void Shuffle()
+    public void Shuffle()
+    {
+        for (int i = Cards.Count - 1; i > 0; i--)
         {
-            for (int i = cards.Count - 1; i > 0; i--)
-            {
-                int j = rng.Next(i + 1);
-                (cards[i], cards[j]) = (cards[j], cards[i]);
-            }
+            int j = rng.Next(i + 1);
+            (Cards[i], Cards[j]) = (Cards[j], Cards[i]);
         }
+    }
 
-        public Card Deal()
-        {
-            Card card = cards[0];
-            cards.RemoveAt(0);
-            return card;
-        }
+    public Card Deal()
+    {
+        Card card = Cards[0];
+        Cards.RemoveAt(0);
+        return card;
     }
 }
 
