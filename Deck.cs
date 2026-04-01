db20b01a26b21d8aabe222ea56bf8f26ba9795fc
Thairan Govender <thairangov@gmail.com>
2026-04-01 16:25:23 +0200

    fixing code

--- Deck.cs
+++ Deck.cs
@@ -1,35 +1,42 @@
-public class Deck
-{
-    public List<Card> Cards { get; private set; }
-    private Random rng = new Random();
+using System;
+using System.Collections.Generic;
 
-    public Deck()
+namespace BlackjackGame
+{
+    public class Deck
     {
-        Cards = new List<Card>();
-        foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+        public List<Card> Cards { get; private set; }
+        private Random rng = new Random();
+
+        public Deck()
         {
-            foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+            Cards = new List<Card>();
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
             {
-                Cards.Add(new Card(suit, rank));
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    Cards.Add(new Card(suit, rank));
+                }
             }
         }
-    }
 
-    public void Shuffle()
-    {
-        for (int i = Cards.Count - 1; i > 0; i--)
+        public void Shuffle()
         {
-            int j = rng.Next(i + 1);
-            (Cards[i], Cards[j]) = (Cards[j], Cards[i]);
+            for (int i = Cards.Count - 1; i > 0; i--)
+            {
+                int j = rng.Next(i + 1);
+                (Cards[i], Cards[j]) = (Cards[j], Cards[i]);
+            }
+        }
+
+        public Card Deal()
+        {
+            Card card = Cards[0];
+            Cards.RemoveAt(0);
+            return card;
         }
     }
-
-    public Card Deal()
-    {
-        Card card = Cards[0];
-        Cards.RemoveAt(0);
-        return card;
-    }
 }
+
 
 
