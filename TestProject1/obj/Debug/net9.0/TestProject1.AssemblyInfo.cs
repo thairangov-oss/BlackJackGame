4ca0f6d8f482c453741f857283e73b2242eb8522
Thairan <thairangov@gmail.com>
2026-04-04 16:12:05 +0200

    commit

--- TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89184046bfd2577ec725a3beaf7571a797a20003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff57105bc084a59ff8144769f08c5772571d66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
