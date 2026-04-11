5e54854fe4a613248a3186833a21f01031de0beb
Thairan <thairangov@gmail.com>
2026-04-11 19:28:10 +0200

    commit

--- BlackJackGame/BlackJackGame.Tests/UnitTest1.cs
+++ BlackJackGame/BlackJackGame.Tests/UnitTest1.cs
@@ -1,10 +1,10 @@
-﻿namespace BlackJackGame.Tests;
+﻿//namespace BlackJackGame.Tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+//public class UnitTest1
+//{
+    //[Fact]
+  //  public void Test1()
+    //{
 
-    }
-}
+     //}
+//}
